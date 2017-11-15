2dc55073b64f131e7ab73ea5b5f4c2119b763754
Darshan Jagathap <Darshan Jagathap>
2017-11-15 09:07:50 +0530

    First commit

---
+++ censortext.js
@@ -0,0 +1,26 @@
+var censoredWords = ["sad", "bad", "mad"];
+var customCensorWords = [];
+
+function censor(inStr){
+	for(idx in censorWords){
+		inStr = inStr.replace(censoredWords[idx]), "***");
+	}
+
+	for(idx in customCensorWords){
+		inStr = inStr.replace(customCensorWords[idx]), "***");
+	}
+
+	return inStr;
+}
+
+function addCensoredWord(word){
+	customCensorWords.push(word);
+}
+
+function getCensoredWords(){
+	return censoredWords.concat(customCensorWords);
+}
+
+exports.censor = censor;
+exports.addCensoredWord = addCensoredWord;
+exports.getCensoredWords = getCensoredWords;
